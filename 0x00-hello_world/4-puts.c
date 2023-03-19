8aa57904aaca85a486cb359a67c4d74d08eceb37
sommypraise <sommypraise20@gmail.com>
2023-03-18 17:37:34 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (void)
+
+
+/**
+ * main - entry point
+ *
+ * Return: 0 when successful
+ */	
+
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return(0);     
+}
